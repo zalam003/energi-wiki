ca72f43f9b09b4abe2ed7e5e07bbd8f82bfd41bf
zalam003 <zaki.alam@gmail.com>
2023-05-28 22:54:06 -0400

    custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Energi Core Gen 3',
   tagline: 'Welcome to the Official Energi Documentation!',
-  url: 'https://zalam003.gitlab.io',
+  url: 'https://energiwiki.smart-corporation.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
